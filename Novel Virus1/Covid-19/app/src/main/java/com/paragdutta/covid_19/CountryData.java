570fb655c6a9d3b6c32edf6b5e4297ae6cb5a370
Parag Dutta <73899758+paragdutta7575@users.noreply.github.com>
2021-05-16 00:44:01 +0530

    Add files via upload

---
+++ Novel Virus1/Covid-19/app/src/main/java/com/paragdutta/covid_19/CountryData.java
@@ -0,0 +1,17 @@
+package com.paragdutta.covid_19;
+
+public class CountryData {
+
+    public  static final String[] countryNames = {
+
+            "IND", "USA", "MY", "AFG", "MF"
+    };
+
+    public  static final int[] countryFlag = {
+
+            R.drawable.india, R.drawable.uk, R.drawable.malesiya, R.drawable.afganistan, R.drawable.maxico
+    };
+
+}
+//different Countries to map their data
+//Project by Parag Dutta
